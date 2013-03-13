b6f1bdd5c04a7bf42b056f76b252755822769e0b
OpenShift guest <andrew12wong@hotmail.com>
2013-03-13 18:47:07 -0400

    didnt really do much i guess

--- MapDisplayer.js
+++ MapDisplayer.js
@@ -1,4 +1,27 @@
-function mazeDrawer(var maze)
+var playerX;
+var playerY;
+
+//mazeDrawer();
+console.log("entered the drawer");
+function mazeDrawer()
 {
     
+    var counter=0;
+    for(var i=0;i<mazeSize;i++)
+    {
+         $("#mapContainer").append('<tr>');
+        for(var j=0;j<mazeSize;j++)
+        {
+           
+            if(maze[j][i]===false)
+            {
+                $("#mapContainer").append('<th id=wall></th>');
+            }
+            if(maze[j][i]===true)
+            {
+                $("#mapContainer").append('<th id=floor></th>');
+            }
+        }
+        $("#mapContainer").append('</tr>');
+    }
 }
--- MazeGenerator.js
+++ MazeGenerator.js
@@ -344,27 +344,7 @@ function deadEnd(cx,cy, counter)
     console.log(endX+", "+endY);
 }
 
-function mazeDrawer()
-{
-    var counter=0;
-    for(var i=0;i<mazeSize;i++)
-    {
-         $("#mapContainer").append('<tr>');
-        for(var j=0;j<mazeSize;j++)
-        {
-           
-            if(maze[j][i]===false)
-            {
-                $("#mapContainer").append('<th id=wall></th>');
-            }
-            if(maze[j][i]===true)
-            {
-                $("#mapContainer").append('<th id=floor></th>');
-            }
-        }
-        $("#mapContainer").append('</tr>');
-    }
-}
+
 
 
 
