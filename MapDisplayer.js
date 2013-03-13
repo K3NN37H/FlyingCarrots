7d337915328a88c5a9d23cc72d2e018041a2a6c3
Ken <kjtchan@gmail.com>
2013-03-13 19:01:38 -0400

    fixed up map displayer so it shows something now

--- MapDisplayer.js
+++ MapDisplayer.js
@@ -1,27 +1,33 @@
 var playerX;
 var playerY;
 
-//mazeDrawer();
+mazeDrawer();
 console.log("entered the drawer");
 function mazeDrawer()
 {
     
     var counter=0;
+    var mapHtml = "";
     for(var i=0;i<mazeSize;i++)
     {
-         $("#mapContainer").append('<tr>');
+        mapHtml += '<tr>';
+        //$("#mapContainer").append('<tr>');
         for(var j=0;j<mazeSize;j++)
         {
            
             if(maze[j][i]===false)
             {
-                $("#mapContainer").append('<th id=wall></th>');
+                mapHtml += '<td class="wall"></td>';
+                //$("#mapContainer").append('<th id=wall></th>');
             }
             if(maze[j][i]===true)
             {
-                $("#mapContainer").append('<th id=floor></th>');
+                mapHtml += '<td class="floor"></td>';
+                //$("#mapContainer").append('<th id=floor></th>');
             }
         }
-        $("#mapContainer").append('</tr>');
+        mapHtml += '</tr>';
+        $("#mapContainer").append(mapHtml);
+        //$("#mapContainer").append('</tr>');
     }
 }
