74af9b5fdd9e9652e2839548b88c8ebd3e9244dd
K3NN37H <kjtchan@gmail.com>
2013-03-27 08:54:26 -0400

    added blackberry xml for building, working slingshot animation functionality

--- BattleSystem.js
+++ BattleSystem.js
@@ -2,8 +2,9 @@ function initBattle()
 {
 	var xSize = $("#battleInterface").width();
 	var ySize = $("#battleInterface").height();
+	$("#battleInterface").show();
 	$("#battleInterface").append('<canvas id="slingshot" width="'+xSize+'px" height="'+ySize+'px"></canvas>');
-	$("#slingshot").on("touchstart touchmove", function(e) {
+	$("#slingshot").on("mousemove", function(e) {
 		//Disable scrolling by preventing default touch behaviour
 		e.preventDefault();
 		var orig = e.originalEvent;
@@ -27,14 +28,30 @@ function initBattle()
 		bcan.lineTo(xSize,10);
 		bcan.stroke();
 	});
-	$("#slingshot").on("touchend", function(e) {
+	$("#slingshot").on("mouseout", function(e) {
 		e.preventDefault();
-		var blah = document.getElementById("can");
+		var blah = document.getElementById("slingshot");
 		var bcan = blah.getContext("2d");
 		bcan.clearRect(0,0,xSize,ySize);
 		bcan.beginPath();
 		bcan.moveTo(0,10);
 		bcan.lineTo(xSize,10);
 		bcan.stroke();
+		var x = e.pageX - $(this).offset().left;
+		var y = e.pageY - $(this).offset().top;
+		//var x = orig.changedTouches[0].pageX - $(this).offset().left;
+		//var y = orig.changedTouches[0].pageY - $(this).offset().top;
+		if ($("#carrot").length === 0)
+		{
+			$("body").append('<div id="carrot" style="position:absolute;background-color:red;"></div>');
+		}
+		$("#carrot").css("left",$("body").width()/2+(xSize/2-x));
+		$("#carrot").css("top",$("#battleInterface").offset().top-(y-10)*2);
 	});
+}
+
+function destroyBattle()
+{
+	$("#battleInterface").empty();
+	$("#carrot").remove();
 }
